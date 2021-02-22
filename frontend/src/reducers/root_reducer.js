a02707b5b9094aea5de9be7eca102d3d0ba31de5
IsoVoyd <dubnersamuel@gmail.com>
2021-02-21 20:22:10 -0800

    finish adding tweets

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import session from "./session_reducer"
 import errors from "./errors_reducer"
+import tweets from "./tweets_reducer"
 
 const rootReducer = combineReducers({
+    tweets,
     session,
     errors
 })
