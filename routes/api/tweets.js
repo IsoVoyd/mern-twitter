0a46501f594f65470f38f53de82823c1c26ca42f
IsoVoyd <dubnersamuel@gmail.com>
2021-02-21 17:56:37 -0800

    add tweet routes

---
+++ routes/api/tweets.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose")
+const passport = require("passport")
+
+const Tweet = require("../../models/Tweet")
+const validateTweetInput = require("../../validation/tweets")
+
+router.get("/", (req, res) => {
+    Tweet.find()
+      .sort({ date: -1 })
+      .then((tweets) => res.json(tweets))
+      .catch((err) =>
+        res.status(404).json({ notweetsfound: "No tweets found" })
+      );
+})
+
+router.get("/user/:user_id", (req, res) => {
+    Tweet.find({user: req.params.user_id})
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(404).json({ notweetsfound: 'No tweets found from that user'}))       
+})
+
+module.exports = router;
