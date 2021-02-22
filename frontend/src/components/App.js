61b3853958b5f6010415852b041d699b99eb480a
IsoVoyd <dubnersamuel@gmail.com>
2021-02-21 19:06:16 -0800

    add routes and navbar

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { AuthRoute, ProtectedRoute } from "../util/route_util"
+import { Switch } from "react-router-dom"
+import NavBarContainer from "./nav/navbar_container"
+
+import MainPage from "./main/main_page"
+import LoginFormContainer from "./login_form_container"
+import SignupFormContainer from "./signup_form_container"
+
+const App = () => {
+    <Switch>
+        <AuthRoute exact path="/" component={MainPage}></AuthRoute>
+        <AuthRoute exact path="/login" component={LoginFormContainer}></AuthRoute>
+        <AuthRoute exact path="/signup" component={SignupFormContainer}></AuthRoute>
+    </Switch>
+}
+
+export default App;
